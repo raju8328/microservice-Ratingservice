5d6adbf3e11d15a7d7add4fee70abd9a7e6c5893
raju8328 <56384471+raju8328@users.noreply.github.com>
2024-05-28 18:54:49 +0530

    commit Rating service

--- src/main/java/com/raju/rating/entities/Rating.java
+++ src/main/java/com/raju/rating/entities/Rating.java
@@ -1,13 +1,14 @@
 package com.raju.rating.entities;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+@Entity
 @Builder
 @Table(name="ratings")
 @Data
